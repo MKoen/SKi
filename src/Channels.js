09b292850c2ae4e86175bb8b5b227adb25798c0c
Koen Missault <koen.missault@telenet.be>
2016-12-08 15:42:10 +0100

    Created map and updated channels

--- src/Channels.js
+++ src/Channels.js
@@ -1,54 +1,112 @@
 import React from 'react';
 import NavBar from './NavBar'
-import { Table, Glyphicon } from 'react-bootstrap'
+import { Table, Glyphicon, Button } from 'react-bootstrap'
 
 var Channels = React.createClass( {
     getInitialState: function () {
         return {};
     },
 
-    render: function () {
-        let channelList = [];
-
-        this.props.route.channels().sort((x, y) => {
-            return y.active - x.active;
-        }).forEach((channel) => {
+    renderActiveChannels: function(){
+        let arrayOfRenderedActiveChannels = [];
+        this.props.getChannels().filter((channel) => channel.active).forEach((channel) => {
             let channelListItem;
+            let water = (channel.water > 0) ? (<Glyphicon glyph="tint"/>) : (<span></span>);
 
-            if (channel.active) {
-                channelListItem = (
-                    <tr key={channel.id}>
-                        <td>{channel.id}</td>
-                        <td>{channel.name}</td>
-                        <td>{channel.position.x}</td>
-                        <td>{channel.position.y}</td>
-                        <td>5</td>
-                        <td>{channel.direction}</td>
-                        <td>{channel.speed}</td>
-                        <td>{channel.temperature}°C</td>
-                        <td>{channel.water}</td>
-                        <td><Glyphicon bsStyle="glyphicon glyphicon-star"/></td>
-                    </tr>
-                )
+            let glyphicon = null;
+            let onClick = null;
+
+            if (this.props.getFavouriteChannelIDs().indexOf(channel.id) != -1) {
+                glyphicon = "star";
+                onClick = this.props.removeIdFromFavourite;
             } else {
-                channelListItem = (
-                    <tr key={channel.id}>
-                        <td>{channel.id}</td>
-                        <td colSpan="8">No data received from this channel...</td>
-                    </tr>
-                )
+                glyphicon = "star-empty";
+                onClick = this.props.addIdToFavourite;
             }
 
-            channelList.push(channelListItem);
+            channelListItem = (
+                <tr key={channel.id}>
+                    <td>{channel.id}</td>
+                    <td>{channel.name}</td>
+                    <td>{channel.position.x} </td>
+                    <td>{channel.position.y} </td>
+                    <td>5</td>
+                    <td><Button onClick={ () => onClick(channel.id)}>
+                        <Glyphicon className="pointer" glyph={glyphicon}/>
+                    </Button></td>
+                </tr>);
+
+            arrayOfRenderedActiveChannels.push(channelListItem);
+            });
+
+        return arrayOfRenderedActiveChannels;
+        },
+
+    renderInactiveChannels: function(){
+        let arrayOfRenderedInactiveChannels = [];
+        this.props.getChannels().filter((channel) => channel.active).forEach((channel) => {
+            let channelListItem;
+
+            channelListItem = (
+                <tr key={channel.id}>
+                    <td>{channel.id}</td>
+                    <td colSpan="8">No data received from this channel...</td>
+                </tr>
+            );
+
+            arrayOfRenderedInactiveChannels.push(channelListItem);
         });
 
+        return arrayOfRenderedInactiveChannels;
+    },
+
+    renderFavouriteChannels: function(){
+        let arrayOfRenderedFavouriteChannels = [];
+        this.props.getChannels().filter((channel) => this.props.getFavouriteChannelIDs().indexOf(channel.id) != -1).forEach((channel) => {
+            let channelListItem;
+            let water = (channel.water > 0) ? (<Glyphicon glyph="tint"/>) : (<span></span>);
+
+            channelListItem = (
+                <tr key={channel.id + "fav"}>
+                    <td>{channel.name}</td>
+                    <td>{channel.position.x}</td>
+                    <td>{channel.position.y}</td>
+                    <td>5</td>
+                    <td>{channel.direction}</td>
+                    <td>{channel.speed}</td>
+                    <td>{channel.temperature}°C</td>
+                    <td>{water}</td>
+                </tr>);
+
+            arrayOfRenderedFavouriteChannels.push(channelListItem);
+        });
+
+        return arrayOfRenderedFavouriteChannels;
+    },
+
+    render: function () {
         return (
             <div>
                 <NavBar/>
-                <Table responsive className="channelList">
+                <Table className="channelList">
                     <thead>
                     <tr>
                         <th>ID</th>
+                        <th>Name</th>
+                        <th>X <Glyphicon onClick={() => this.props.sortChannelNumericalBy("x")}  className="pointer" glyph="sort-by-order"/></th>
+                        <th>Y <Glyphicon onClick={() => this.props.sortChannelNumericalBy("y")}  className="pointer" glyph="sort-by-order"/></th>
+                        <th>Distance <Glyphicon onClick={() => this.props.sortChannelNumericalBy("distance")} className="pointer" glyph="sort-by-order"/></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderActiveChannels()}
+                        {this.renderInactiveChannels()}
+                    </tbody>
+                </Table>
+                <Table className="favouritesList">
+                    <thead>
+                    <tr>
                         <th>Name</th>
                         <th>X</th>
                         <th>Y</th>
@@ -57,11 +115,10 @@ var Channels = React.createClass( {
                         <th>Speed</th>
                         <th>Temperature</th>
                         <th>Water</th>
-                        <th></th>
                     </tr>
                     </thead>
                     <tbody>
-                        {channelList}
+                        {this.renderFavouriteChannels()}
                     </tbody>
                 </Table>
             </div>
